ae1a0cc91788d73d13ef122180d6f1058fa3902f
Nega0619 <80737049+Nega0619@users.noreply.github.com>
2022-09-01 14:12:51 +0900

    z-index로 인한 오류 수정

--- suite_2csv.py
+++ suite_2csv.py
@@ -29,8 +29,7 @@ def get_xlsx(file_path, project_name='ISON'):
 
     meta_file_list = get_all_metaFiles(meta_path)
 
-
-    # meta 파일에서 필요한 정보 가져오기
+    # meta 폴더에서 json 정보 가져오기
     for i, mf in enumerate(meta_file_list):
         df.loc[i] = ''
         with open(mf) as json_meta_data:
@@ -40,7 +39,7 @@ def get_xlsx(file_path, project_name='ISON'):
         df.loc[i]['tags']       = json_meta_data['tags'][0]['name']
         df.loc[i]['work_assignee'] = json_meta_data['work_assignee']
 
-        # label 파일에서 필요한 정보 가져오기
+        # label 폴더에서 json 정보 가져오기
         label_path = source_path + json_meta_data['label_path'][0]
         with open(label_path) as json_label_data:
             json_label_data = json.load(json_label_data)
@@ -48,23 +47,21 @@ def get_xlsx(file_path, project_name='ISON'):
         # object 개수대로 pandas의 열 수 늘려주기
         object_num = len(json_label_data['objects'])
         for num in range(object_num):
-            z_index    = json_label_data['objects'][num]['annotation']['meta']['z_index']
+            index_num = num+1
+            # index_num    = json_label_data['objects'][num]['annotation']['meta']['index_num']
             class_name = json_label_data['objects'][num]['class_name']
             properties = json_label_data['objects'][num]['properties']
             properties = '' if not properties else properties[0]['option_name']
 
-
             # dataframe에 해당 컬럼이없으면 추가해주기
-            if 'class'+str(z_index) not in df.columns:
-                df['class'+str(z_index)] = ''
-                df['property'+str(z_index)] = ''
-                print(f'{z_index}번 클래스 추가')
+            if 'class'+str(index_num) not in df.columns:
+                df['class'+str(index_num)] = ''
+                df['property'+str(index_num)] = ''
+                print(f'{index_num}번 클래스 추가')
             
-            df.loc[i]['class'+str(z_index)] = class_name
-            df.loc[i]['property'+str(z_index)] = properties
+            df.loc[i]['class'+str(index_num)] = class_name
+            df.loc[i]['property'+str(index_num)] = properties
     
     df.to_excel(source_path+project_name+'.xlsx')
 
     return True
-
-
