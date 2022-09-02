7575024b640b1c126ee87ddd6fc5b3a9980ab01e
Nega0619 <80737049+Nega0619@users.noreply.github.com>
2022-09-02 09:11:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from suite_2csv import *
 def get_filepath():
     file_path=textExample.get("1.0","end-1c")
     # btnRead['text'] = 'ing'
-    lbl.config(text = "Successfully file made in path.")
+    # lbl.config(text = c"Successfully file made in path.")
     # btnRead['state'] = tk.DISABLED
-    result = get_xlsx(file_path)
+    ##################################################
+    file_path = '/home/hwi/suite_2_excel/ISON 2022-08-31 14_05_51_FOR_CODE'
+    ##################################################
+    result, path = make_xlsx(file_path)
     if result:
         btnRead['text'] = 'Done!'
         btnRead['state'] = tk.ACTIVE
         btnRead['command'] = killer
+        lbl.config(text = path)
+
 
 def killer():
     exit()
@@ -29,7 +34,7 @@ if __name__=="__main__":
     btnRead=tk.Button(root, height=1, text="then I will make you a xlsx file", font=20,command=get_filepath)
     btnRead.pack()
 
-    # # Label Creation
+    # Label Creation
     lbl = tk.Label(root, text = "")
     lbl.pack()
 
